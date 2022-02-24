2f5575d7800729511953ab7791759fa93ab6bc5c
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-24 15:50:14 -0800

    Add keydown events that allow user to rotate the car with arrow keys. #2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 /* global data */
 /* exported data */
+
+var $car = document.querySelector('#car');
+
+function handleArrowKeys(event) {
+  if (event.key === 'ArrowLeft') {
+    $car.setAttribute('class', 'dir-left');
+  } else if (event.key === 'ArrowRight') {
+    $car.setAttribute('class', 'dir-right');
+  } else if (event.key === 'ArrowUp') {
+    $car.setAttribute('class', 'dir-up');
+  } else if (event.key === 'ArrowDown') {
+    $car.setAttribute('class', 'dir-down');
+  }
+}
+
+document.addEventListener('keydown', handleArrowKeys);
